b6f07b7a491a0f4249f9691fd818715166787708
Mees van Dijk <mees@itisme.es>
2017-05-06 01:45:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const licenses = require('./licenses.json')
+const inquirer = require('inquirer')
+const existsSync = require('exists-sync')
+
+if (existsSync('./LICENSE')) {
+  console.log('Already found a license. Aborting ❌')
+  process.exit(0)
+}
+
+const preCollected = {
+  name: null,
+  licenseIndex: null
+}
+
+const licenseChoices = [
+      {
+        name: 'MIT',
+        value: licenses.MIT
+      },
+      {
+        name: 'GPL-3.0',
+        value: licenses.GPL
+      },
+      {
+        name: 'Apache-2.0',
+        value: licenses.Apache
+      }
+    ]
+
+try {
+  const pack = require('./package.json')
+  if (pack.author) {
+    preCollected.name = pack.author
+  }
+  if (pack.license) {
+    for (const [index, choice] of licenseChoices.entries()) {
+      if (pack.license === choice.name) {
+        preCollected.licenseIndex = index
+        break
+      }
+    }
+  }
+} catch (e) {
+  console.log(e)
+} // do nothing
+
+inquirer.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    default: preCollected.name || undefined,
+    message: 'enter your name:'
+  },
+  {
+    type: 'input',
+    name: 'year',
+    message: 'enter the year:',
+    default: new Date().getFullYear(),
+    validate: str => /^[0-9]{4,}$/.test(str)
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'pick a license:',
+    default: preCollected.licenseIndex || 0,
+    choices: licenseChoices
+  }
+])
+.then(answers => {
+  // fill in the name and year
+  answers.license = answers.license.replace('<name>', answers.name)
+  answers.license = answers.license.replace('<year>', answers.year)
+
+  // write the license
+  fs.writeFileSync('./LICENSE', answers.license)
+  console.log('Done! 👌')
+})
+.catch(e => {
+  console.error('Something bad happened 😨')
+  if (process.env.DEBUG.includes('lick')) {
+    console.error(e)
+  }
+})
